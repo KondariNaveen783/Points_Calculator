28eff4a66c11f0431c5455f59094bdaad5e6f1b8
KondariNaveen783 <kondarinaveen5@gmail.com>
2025-01-29 15:19:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
+import React from "react";
+import PointsCalculator from "./components/PointsCalculator";
 
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Customer Points Calculator</h1>
+            <PointsCalculator />
+        </div>
+    );
+};
+
+export default App;
