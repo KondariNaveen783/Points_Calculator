831875c6e8fabb74271d4463ba4c418f1681e40f
KondariNaveen783 <kondarinaveen5@gmail.com>
2025-01-29 15:16:48 +0530

    Update pointsCalculator.js

--- src/utils/pointsCalculator.js
+++ src/utils/pointsCalculator.js
@@ -1,1 +1,12 @@
+const calculatePoints = (amount) => {
+    let points = 0;
+    if (amount > 50) {
+        points += (amount - 50) * 1;
+    }
+    if (amount > 100) {
+        points += (amount - 100) * 1;
+    }
+    return points;
+};
 
+export { calculatePoints };
