e55b91fa983f4a85c2404bd81c2dd3758220be82
KondariNaveen783 <kondarinaveen5@gmail.com>
2025-01-29 15:15:34 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,16 @@
+const fetchTransactionData = async () => {
+    try {
+        const response = await fetch('/data/transactions.json'); 
+        
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Failed to fetch transaction data:', error);
+        return [];
+    }
+};
 
+export { fetchTransactionData };
